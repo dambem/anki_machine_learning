edae06afcb8a1a03fda65da562ae51710fab0fc3
bembendamian@yahoo.co.uk <bembendamian@yahoo.co.uk>
2019-12-10 14:12:06 +0000

    Rewriting for lite version

--- example.py
+++ example.py
@@ -1,12 +1,16 @@
 from overdrive import Overdrive
+import tkinter
+
+top = tkinter.Tk()
 
 def locationChangeCallback(addr, location, piece, speed, clockwise):
     # Print out addr, piece ID, location ID of the vehicle, this print everytime when location changed
     print("Location from " + addr + " : " + "Piece=" + str(piece) + " Location=" + str(location) + " Clockwise=" + str(clockwise))
 
 
-car = Overdrive("xx:xx:xx:xx:xx:xx")
+car = Overdrive("F6:ED:77:8C:70:CA")
 car.setLocationChangeCallback(locationChangeCallback) # Set location change callback to function above
-car.changeSpeed(500, 1000) # Set car speed with speed = 500, acceleration = 1000
+car.changeSpeed(750, 1000) # Set car speed with speed = 500, acceleration = 1000
 car.changeLaneRight(1000, 1000) # Switch to next right lane with speed = 1000, acceleration = 1000
-input() # Hold the program so it won't end abruptly
+
+top.mainloop()
